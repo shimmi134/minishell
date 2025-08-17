1afd4a31c3b010194f471e14afbacecfe6ada1f1
Sam <sami.himi06@gmail.com>
2025-08-17 21:56:58 +0200

    Norme on do_command function

--- test.c
+++ test.c
@@ -56,74 +56,320 @@ void free_penv(char **penv)
 	free(penv);
 }
 
-void do_commands(t_shell *elem, t_env **env, int ac)
-{
-    int *pids = calloc(ac+1, sizeof(int));
-    int prev_fd = -1;
-    int old_stdout = dup(STDOUT_FILENO);
-    char **penv = NULL;
-    int count = 0;
-    int *last_status_ptr = NULL;
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdio.h>
 
+
+static void set_out_flags(int *flags, int append)
+{
+    *flags = 0;
+    if (append)
+    {
+        *flags = O_WRONLY | O_CREAT | O_APPEND;
+        return;
+    }
+    *flags = O_WRONLY | O_CREAT | O_TRUNC;
+}
+
+static void open_and_dup_outfile(const char *path, int append)
+{
+    int flags;
+    int fd;
+    int res;
+    flags = 0;
+    fd = -1;
+    res = 0;
+    set_out_flags(&flags, append);
+    fd = open(path, flags, 0644);
+    if (fd < 0)
+    {
+        perror(path);
+        exit(1);
+    }
+    res = dup2(fd, STDOUT_FILENO);
+    if (res < 0)
+    {
+        perror(path);
+        exit(1);
+    }
+    close(fd);
+}
+
+static int run_standalone_builtin(t_shell *elem, t_env **env)
+{
+    int code;
+    code = 0;
+    if (elem->command->outfile)
+        open_and_dup_outfile(elem->command->outfile,
+                             elem->command->append);
+    code = do_builtins(elem, env);
+    if (elem->exit_status_code)
+        *(elem->exit_status_code) = code;
+    return code;
+}
+
+static void prepare_pipe_if_needed(int next_pipe[2], int need)
+{
+    int res;
+    res = 0;
+    next_pipe[0] = -1;
+    next_pipe[1] = -1;
+    if (need)
+    {
+        res = pipe(next_pipe);
+        if (res < 0)
+        {
+            perror("pipe");
+            exit(1);
+        }
+    }
+}
+
+static void dup_prev_to_stdin(int prev_fd)
+{
+    int res;
+    res = 0;
+    if (prev_fd != -1)
+    {
+        res = dup2(prev_fd, STDIN_FILENO);
+        if (res < 0)
+        {
+            perror("dup2");
+            exit(1);
+        }
+    }
+}
+
+static void dup_next_to_stdout(int next_write)
+{
+    int res;
+    res = 0;
+    if (next_write != -1)
+    {
+        res = dup2(next_write, STDOUT_FILENO);
+        if (res < 0)
+        {
+            perror("dup2");
+            exit(1);
+        }
+    }
+}
+
+static void child_close_unneeded_fds(int prev_fd, int next_read, int next_write)
+{
+    if (prev_fd != -1) close(prev_fd);
+    if (next_read != -1) close(next_read);
+    if (next_write != -1) close(next_write);
+}
+
+static void child_handle_infile(t_shell *elem)
+{
+    int fd;
+    int res;
+    fd = -1;
+    res = 0;
+    if (elem->command->infile)
+    {
+        fd = open(elem->command->infile, O_RDONLY);
+        if (fd < 0)
+        {
+            perror(elem->command->infile);
+            exit(1);
+        }
+        res = dup2(fd, STDIN_FILENO);
+        if (res < 0)
+        {
+            perror(elem->command->infile);
+            exit(1);
+        }
+        close(fd);
+    }
+}
+
+static void child_handle_outfile(t_shell *elem)
+{
+    int fd;
+    int flags;
+    fd = -1;
+    flags = 0;
+    if (elem->command->outfile)
+    {
+        set_out_flags(&flags, elem->command->append);
+        fd = open(elem->command->outfile, flags, 0644);
+        if (fd < 0)
+        {
+            perror(elem->command->outfile);
+            exit(1);
+        }
+        if (dup2(fd, STDOUT_FILENO) < 0)
+        {
+            perror(elem->command->outfile);
+            exit(1);
+        }
+        close(fd);
+    }
+}
+
+static void child_exec_or_builtin(t_shell *elem, t_env **env)
+{
+    char **penv;
+    penv = NULL;
+    if (elem->command->cmd != NULL)
+    {
+        penv = create_envp(*env);
+        exec_command(elem, env, penv);
+        perror("exec");
+        exit(1);
+    }
+    else
+    {
+        int code;
+        code = do_builtins(elem, env);
+        exit(code);
+    }
+}
+
+static void child_process(t_shell *elem, t_env **env, int prev_fd,
+                          int next_write)
+{
+    dup_prev_to_stdin(prev_fd);
+    dup_next_to_stdout(next_write);
+    child_close_unneeded_fds(prev_fd, -1, next_write);
+    child_handle_infile(elem);
+    child_handle_outfile(elem);
+    child_exec_or_builtin(elem, env);
+}
+
+static void parent_housekeeping(int *prev_fd, int next_read, int next_write)
+{
+    if (*prev_fd != -1)
+    {
+        close(*prev_fd);
+    }
+    if (next_write != -1)
+    {
+        close(next_write);
+    }
+    if (next_read != -1)
+    {
+        *prev_fd = next_read;
+    }
+    else
+    {
+        *prev_fd = -1;
+    }
+}
+
+static pid_t spawn_one_process(t_shell *elem, t_env **env, int *prev_fd)
+{
+    int next_pipe[2];
+    pid_t pid;
+    int need_next;
+    int next_read;
+    int next_write;
+    next_read = -1;
+    next_write = -1;
+    next_pipe[0] = -1;
+    next_pipe[1] = -1;
+    need_next = 0;
+    if (elem->next != NULL) need_next = 1;
+    prepare_pipe_if_needed(next_pipe, need_next);
+    if (need_next) next_read = next_pipe[0];
+    if (need_next) next_write = next_pipe[1];
+    pid = fork();
+    if (pid < 0) perror("fork");
+    if (pid < 0) exit(1);
+    if (pid == 0)
+    {
+        if (next_read != -1) close(next_read);
+        child_process(elem, env, *prev_fd, next_write);
+        exit(1);
+    }
+    parent_housekeeping(prev_fd, next_read, next_write);
+    return pid;
+}
+
+
+static void wait_children_and_set_status(int *pids, int count,
+                                         int *last_status_ptr)
+{
+    int i;
+    int status;
+    i = 0;
+    status = 0;
+    while (i < count)
+    {
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status))
+        {
+            if (last_status_ptr)
+            {
+                *last_status_ptr = WEXITSTATUS(status);
+            }
+        }
+        else
+        {
+            if (last_status_ptr)
+            {
+                *last_status_ptr = 127;
+            }
+        }
+        i = i + 1;
+    }
+}
+
+/* execute_loop does the per-element loop; <= 25 lines */
+static int execute_loop(t_shell *elem, t_env **env, int *pids,
+                        int **last_status_ptr_out)
+{
+    int prev_fd, count;
+    prev_fd = -1;
+    count = 0;
     while (elem && elem->type)
     {
-        bool has_next = (elem->next != NULL);
-        bool is_cmd = (strncmp(elem->type, "command", 7) == 0);
-        bool is_builtin = (strncmp(elem->type, "built-in", 9) == 0);
-        if (is_builtin && !has_next)
+        if (strncmp(elem->type, "built-in", 9) == 0 && elem->next == NULL)
         {
-			do_outfile(elem->command->outfile, elem->command->append);
-            int code = do_builtins(elem, env);
-            *(elem->exit_status_code) = code;
-            last_status_ptr = elem->exit_status_code;
+            run_standalone_builtin(elem, env);
+            *last_status_ptr_out = elem->exit_status_code;
             elem = elem->next;
             continue;
         }
-        int next_pipe[2] = {-1, -1};
-        if (has_next)
-        {
-            if (pipe(next_pipe) < 0)
-                perror("pipe"), exit(1);
-        }
-        int pid = fork();
-        if (pid < 0)
-            perror("fork"), exit(1);
-        if (pid == 0)
-        {
-			close_and_open_child(prev_fd, has_next, next_pipe);
-			do_infile(elem->command->infile);
-            do_outfile(elem->command->outfile, elem->command->append);
-            if (elem->command->cmd != NULL && is_cmd) // Have to use execute_command function in exec utils
-            {
-                penv = create_envp(*env);
-                exec_command(elem, env, penv);
-            }
-            else if (is_builtin)// built-in in pipeline
-                exit(do_builtins(elem, env));
-        }
-        else
-        {
-            pids[count++] = pid;
-			prev_fd = close_parent(prev_fd, has_next, next_pipe);
-        }
-        last_status_ptr = elem->exit_status_code;
+        pid_t pid;
+        pid = spawn_one_process(elem, env, &prev_fd);
+        pids[count] = (int)pid;
+        count = count + 1;
+        *last_status_ptr_out = elem->exit_status_code;
         elem = elem->next;
     }
     if (prev_fd != -1) close(prev_fd);
-    for (int i = 0; i < count; i++)
-    {
-        int status;
-        waitpid(pids[i], &status, 0);
-        if (WIFEXITED(status))
-            *last_status_ptr = WEXITSTATUS(status);
-        else
-            *last_status_ptr = 127;
-    }
-    dup2(old_stdout, STDOUT_FILENO);
-    close(old_stdout);
-    free(pids);
-    if (penv) free_penv(penv);
+    return count;
 }
+
+
+void do_commands(t_shell *elem, t_env **env, int ac)
+{
+    int *pids;
+    int old_stdout;
+    int *last_status_ptr;
+    int count;
+    pids = NULL;
+    old_stdout = -1;
+    last_status_ptr = NULL;
+    count = 0;
+    pids = calloc(ac + 1, sizeof(int));
+    old_stdout = dup(STDOUT_FILENO);
+    count = execute_loop(elem, env, pids, &last_status_ptr);
+    wait_children_and_set_status(pids, count, last_status_ptr);
+    if (old_stdout != -1) dup2(old_stdout, STDOUT_FILENO);
+    if (old_stdout != -1) close(old_stdout);
+    free(pids);
+}
+
 
 
 int	env_len(t_env *env)
@@ -330,7 +576,6 @@ int	main(int argc, char *argv[], char *envp[])
 	t_cmd		*t_head;
 	t_env		*env;
 	t_cmd		*hd_temp;
-	int			heredoc_exit_status;
 	int			hd_res;
 	int			*exit_status;
 	t_heredoc	*heredoc;
