8b39dacbe5f3ab64594a5e5b0ae712e975a02d13
Sami Himi Ben Alilou <shimi-be@cbr8s5.42barcelona.com>
2025-08-24 17:22:15 +0200

    Solved exit with sign

--- test_utils12.c
+++ test_utils12.c
@@ -6,20 +6,31 @@
 /*   By: shimi-be <shimi-be@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 16:55:28 by shimi-be          #+#    #+#             */
-/*   Updated: 2025/08/24 17:03:31 by shimi-be         ###   ########.fr       */
+/*   Updated: 2025/08/24 17:21:28 by shimi-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	is_sign(int c)
+{
+	return (c == '+' || c == '-');
+}
+
 int	is_sal(char *str)
 {
 	int	i;
+	int	flag;
 
 	i = 0;
+	flag = 0;
 	while (str[i])
 	{
-		if(!ft_isdigit(str[i]))
+		if (i == 1)
+			flag = 1;
+		if (is_sign(str[i]) && flag)
+			return (0);
+		if(!ft_isdigit(str[i]) && !is_sign(str[i]))
 			return (0);
 		i++;
 	}
