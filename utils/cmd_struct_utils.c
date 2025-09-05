7ed477fa218ac8489332eb324a2293fed768a168
joe <142997450+joshapir@users.noreply.github.com>
2025-09-05 20:09:48 +0200

    fixed token concat issue

--- utils/cmd_struct_utils.c
+++ utils/cmd_struct_utils.c
@@ -6,7 +6,7 @@
 /*   By: joshapir <joshapir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 18:53:48 by joshapir          #+#    #+#             */
-/*   Updated: 2025/09/04 18:22:21 by shimi-be         ###   ########.fr       */
+/*   Updated: 2025/09/05 20:08:41 by joshapir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	cmd_loop(t_token **tokens, t_cmd **cmds, int type, t_env *envp)
 			i++;
 		i = i - 1;
 		type = (*tokens)->type;
-		if (i > 0 && !(*tokens)->new_word && type != TOKEN_VARIABLE)
+		if (i > 0 && !(*tokens)->new_word && type != TOKEN_VARIABLE && !(*cmds)->infile && !(*cmds)->outfile)
 			handle_join(*cmds, i);
 		if ((*tokens)->type == TOKEN_VARIABLE)
 			*tokens = (*tokens)->next;
